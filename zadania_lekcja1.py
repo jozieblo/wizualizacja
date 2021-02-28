5d9e5b34c1a956fcce590478e42732dbae437df0
jozieblo <73527284+jozieblo@users.noreply.github.com>
2021-02-28 21:58:12 +0100

    Add files via upload

---
+++ zadania_lekcja1.py
@@ -0,0 +1,74 @@
+#zad1
+print("zad1")
+napis1="Jest to zmienna typu napis"
+liczba1=21
+print(napis1)
+print(liczba1)
+
+#zad2
+print("zad2")
+dodawanie= 10 + 5
+odejmowanie= 10 - 5
+mnożenie= 10 * 5
+dzielenie=10/5
+print(dodawanie)
+print(odejmowanie)
+print(mnożenie)
+print(dzielenie)
+
+#zad3
+print("zad3")
+liczba1=20
+print(liczba1)
+liczba1+=5
+print(liczba1)
+liczba1-=5
+print(liczba1)
+liczba1*=5
+print(liczba1)
+liczba1/=5
+print(liczba1)
+liczba1**=5
+print(liczba1)
+liczba1//=5
+print(liczba1)
+#zad4
+print("zad4")
+import math
+wynik1 = (math.log(5 + (math.sin(8) * 2))) * (1 / 6)
+print(wynik1)
+
+print(math.floor(3.55))
+print(math.ceil(4.80))
+#zad5
+print("zad5")
+imie="JAREK"
+nazwisko="OZIEBLO"
+cap1=imie.capitalize()
+cap2=nazwisko.capitalize()
+print(cap1+ " "+cap2)
+
+#zad6
+print("zad6")
+ulala="lalalala cos tam cos"
+count=ulala.count("la")
+print("Ilosc la jest rowna", count)
+
+#zad7
+print("zad7")
+indeksy="Skamieliny, ta ta ta"
+ind=indeksy[0],indeksy[19]
+print(ind)
+
+#zad8
+print("zad8")
+podzial=ulala.split("a",2)
+print(podzial)
+
+#zad9
+print("zad9")
+napis="to jest napis w python"
+print(napis)
+print(float(2.25))
+liczba5=256
+print(hex(liczba5))
