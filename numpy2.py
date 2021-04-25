5137acf85e47eef38b9d48e0b398bac2df1a4af8
jozieblo <73527284+jozieblo@users.noreply.github.com>
2021-04-25 12:03:43 +0200

    Add files via upload

---
+++ numpy2.py
@@ -0,0 +1,85 @@
+# import numpy as np
+#
+# print("zad1")
+#
+# a=np.arange(3)
+# b=np.array([7,8,9])
+# print(a)
+# print(b)
+# print(" ")
+# print(a.dot(b))
+
+# print("zad2")
+#
+# c=np.arange(9).reshape(3,3)
+# d=np.arange(16).reshape(4,4)
+# print(c)
+# print(" ")
+# print(d)
+# print(" ")
+# print(c.min(axis=0))
+# print(" ")
+# print(c.min(axis=1))
+# print(" ")
+# print(d.min(axis=0))
+# print(" ")
+# print(d.min(axis=1))
+
+# print("zad3")
+# c=a*b
+# print(c)
+
+# print("zad4")
+#
+# e=np.arange(3,dtype='int32')
+# f=np.array([3.25,8.96,6.01],dtype='float')
+# print(e)
+# print(f)
+# print(e.dot(f))
+
+# print("zad5")
+# g=np.array([[5,8,10],[3,2,1]])
+# h=np.zeros((2,3))
+# print(g)
+# g=np.sin(g)
+# print(np.add(g,h))
+
+# print("zad6")
+# i=np.array([[1,2,6],[8,12,21]])
+# j=np.zeros((2,3))
+# print(i)
+# i=np.cos(i)
+# print(np.add(i,j))
+
+# print("zad7")
+# k=np.arange(5)
+# l=np.arange(5)
+# print(k)
+# print(l)
+# suma=0
+# for x in k:
+#     suma+=k[x]+l[x]
+# print(suma)
+
+# print("zad8")
+# m=np.arange(9).reshape(3,3)
+# for x in m:
+#    print(x)
+
+# print("zad9")
+# m=np.arange(9).reshape(3,3)
+# for x in m.flat:
+#     print(x)
+
+# print("zad10")
+# m=np.arange(81).reshape(9,9)
+# print(m)
+# print(m.shape)
+# print(" ")
+# n=m.ravel()
+# print(n)
+# print(n.shape)
+# print(" ")
+# o=m.T
+# print(o)
+# print(o.shape)
